c9f6869d2f7bfe603e84ab0c347cef200bf47371
pawel <pawel@domena.pl>
2016-03-04 23:38:37 +0100

    [scalafp1] updated examples for composition exercise

--- src/main/scala/jug/lodz/workshops/fp1/answers/ScalaFP1ComposeFunctionsAnswer.scala
+++ src/main/scala/jug/lodz/workshops/fp1/answers/ScalaFP1ComposeFunctionsAnswer.scala
@@ -5,25 +5,41 @@ package jug.lodz.workshops.fp1.answers
   */
 object ScalaFP1ComposeFunctionsAnswer {
 
-  val addOne:Int=>Int = x=>x+1
-
-  val multiplyByTwo : Int=>Int = x=> x*2
+  val parse : String => Int = s=>s.toInt
+  val neighbours:Int=>List[Int] = x=>List(x-1,x+1)
 
   def main(args: Array[String]) {
-    val andThenResult=addOne.andThen(multiplyByTwo)(1)
-    println(andThenResult)
+    //DEMONSTRATION
+    println("DEMONSTRATION : AndThen & compose")
+    println(parse.andThen(neighbours)("3") == List(2,4))
+    println(neighbours.compose(parse)("5") == List(4,6))
 
-    val composeResult=addOne.compose(multiplyByTwo)(1)
-    println(composeResult)
+    //LAB
+    println("LAB : andThen & compose")
+    println(customAndThen(parse,neighbours)("4")==List(3,5))
+    println(customCompose(neighbours,parse)("9")==List(8,10))
 
-    //lab
-    println("customAndThen : "+customAndThen(addOne,multiplyByTwo)(1))
-    println("customCompose : "+customCompose(addOne,multiplyByTwo)(1))
+    println("LAB : generics")
+    println(customAndThenGeneric(parse,neighbours)("4")==List(3,5))
+    println(customComposeGeneric(neighbours,parse)("9")==List(8,10))
+
+    println("LAB : andThenSeq")
+    println(andThenSeq(Seq(x=>x+1,x=>x*2,x=>x+5))(1)==9)
+    println(andThenSeq(Seq(x=>x+1,x=>x*2,x=>x+5))(3)==13)
+
   }
 
-  def customAndThen(f1:Int=>Int, f2:Int=>Int):Int=>Int = x=>f2(f1(x))
+  def customAndThen(f1:String=>Int, f2:Int=>List[Int]):String=>List[Int] = x=>f2(f1(x))
+  def customCompose(f1:Int=>List[Int], f2:String=>Int):String=>List[Int] = x=>f1(f2(x))
 
+  //ADDITIONAL
+  def customAndThenGeneric[A,B,C](f1:A=>B, f2:B=>C):A=>C = x=>f2(f1(x))
+  def customComposeGeneric[A,B,C](f1:B=>C, f2:A=>B):A=>C = x=>f1(f2(x))
 
-  def customCompose(f1:Int=>Int, f2:Int=>Int):Int=>Int = x=>f1(f2(x))
+  //ADDITIONAL 2
+  def andThenSeq(fs:Seq[Int=>Int]): Int=>Int = fs reduce customAndThenGeneric[Int,Int,Int]
+//  def andThenSeq(fs:Seq[Int=>Int]): Int=>Int = fs reduce ((f1,f2)=>f1 andThen f2)
+//  def andThenSeq(fs:Seq[Int=>Int]): Int=>Int = fs reduce (_ andThen _)
+
 
 }
