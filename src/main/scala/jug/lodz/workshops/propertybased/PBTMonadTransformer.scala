09bcdc466821236e7e01febb5194a0ac5745d53b
pawel <pawel@domena.pl>
2016-03-06 14:35:30 +0100

    [PBT] MonadT

---
+++ src/main/scala/jug/lodz/workshops/propertybased/PBTMonadTransformer.scala
@@ -0,0 +1,59 @@
+package jug.lodz.workshops.propertybased
+
+import java.util.concurrent.TimeUnit
+
+
+/**
+  * Created by pawel on 06.03.16.
+  */
+object PBTMonadTransformer {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+
+
+  case class Company(id:Int,name: String, info:String)
+
+  def service1(name:String) : Future[Option[Int]] ={
+    val companiesIds:Map[String,Int] = Map(
+      "company1" -> 1,
+      "company2" -> 2
+    )
+
+    Future{
+      TimeUnit.MILLISECONDS.sleep(500)
+      companiesIds.get(name)
+    }
+  }
+
+
+  def service2(id:Int) : Future[Option[Company]] ={
+    val companies:Map[Int,Company] = Map(
+       1 -> Company(1, "company1", "some info about company1"),
+       2 -> Company(2, "company2", "some info about company2")
+    )
+
+    Future{
+      TimeUnit.MILLISECONDS.sleep(500)
+      companies.get(id)
+    }
+  }
+
+  def main(args: Array[String]) {
+    import cats.data.OptionT
+    import cats.std.future._
+
+    val result=for{
+      id<-OptionT(service1("company1"))
+      company<-OptionT(service2(id))
+    } yield company.info
+
+    val finalValue: Future[Option[String]] = result.value
+
+    finalValue.onSuccess{
+      case Some(v) => println(v)
+    }
+    TimeUnit.SECONDS.sleep(2)
+  }
+
+}
