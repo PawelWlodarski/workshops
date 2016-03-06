444317a7ae33052c928666456d78967450fc96a9
pawel <pawel@domena.pl>
2016-03-06 15:20:52 +0100

    [PBT] Kleisli

---
+++ src/main/scala/jug/lodz/workshops/propertybased/PBTKleisli.scala
@@ -0,0 +1,23 @@
+package jug.lodz.workshops.propertybased
+
+
+/**
+  * Created by pawel on 06.03.16.
+  */
+object PBTKleisli {
+
+
+
+  def main(args: Array[String]) {
+    import cats.data.Kleisli
+    import cats.std.all._
+    val split:String=>List[String] = s=>s.split(" ").toList
+    val variants:String=>List[String] = s=>List(s,s.toUpperCase,s.toLowerCase)
+
+    val variantsK = Kleisli(split).andThen(variants)
+
+    println(variantsK.run("this is a sentence"))
+    //List(this, THIS, this, is, IS, is, a, A, a, sentence, SENTENCE, sentence)
+
+  }
+}
