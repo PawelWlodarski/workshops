41c6a10cbe4308ed5b5dd6166265cf4403510ff3
Pawel Wlodarski <pwlodarski@rule.lan>
2016-03-11 12:35:58 +0100

    [javafp2] lifting and unification

---
+++ src/main/java/workshops/functional/fp2/exercises/ProcessCollection6.java
@@ -0,0 +1,81 @@
+package workshops.functional.fp2.exercises;
+
+import workshops.functional.fp2.answers.LoggerModuleAnswer;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Created by pwlodarski on 2016-03-09.
+ */
+public class ProcessCollection6 {
+
+    static Consumer<String> logger = LoggerModuleAnswer.defaultLogger;
+
+    static <B> Collection<B> queryForMostUsages(Function<Collection<String>,Collection<String>> extractFields,
+                                          Function<Collection<String>,Collection<B>> createSummary){
+        try{
+            List<String> lines= Files.readAllLines(
+                    Paths.get(
+                            ClassLoader.getSystemResource("fpjava/purchases.csv").toURI()
+                    )
+            );
+
+            lines.remove(0); //header
+            Collection<String> fields=extractFields.apply(lines);
+            return createSummary.apply(fields);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList();
+        }
+    }
+
+    //functions
+    static Function<Integer,Function<String,String>> extractField= index->line->line.split(",")[index];
+    static Function<String,String> extractUser=extractField.apply(0);
+
+    static Function<Collection<String>,Map<String,Integer>> fieldsSummary = fields->{
+        Map<String,Integer> counts=new HashMap<>();
+        for (String field : fields) {
+            counts.compute(field, (k,v)-> v==null? 1 : v+1);
+        }
+        return counts;
+    };
+
+    static Function<Map<String,Integer>, Collection<String>> joinSorted = input ->
+            input.entrySet().stream()
+                    .sorted((a,b) -> b.getValue().compareTo(a.getValue()))
+                    .map(entry -> entry.getKey() + ":" + entry.getValue())
+                    .collect(Collectors.toList());
+
+
+    static Function<Collection<String>,Collection<String>> transformResult= fieldsSummary.andThen(joinSorted);
+
+
+    //lab
+    static Function<Collection<String>,Collection<String>> linesToUsers =FunctionalLibraryV1.liftToCollection(extractUser);
+
+
+    public static void main(String[] args){
+        queryForMostUsages(linesToUsers,transformResult).forEach(logger);
+    }
+
+
+
+}
+//LAB
+class FunctionalLibraryV1{
+    //LAB
+    public static <A,B> Function<Collection<A>,Collection<B>> liftToCollection(Function<A,B> f){
+        throw new UnsupportedOperationException("lab not implemented");
+    }
+
+    //ADDITIONAL
+    public static <A,B> Function<Optional<A>,Optional<B>> liftToOptional(Function<A,B> f){
+        throw new UnsupportedOperationException("lab not implemented");
+    }
+}
