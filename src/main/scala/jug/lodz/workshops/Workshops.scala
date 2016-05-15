caf0ef2a8529d5940450e0af07b67fe390b82c68
pawel <pawel@domena.pl>
2016-05-15 11:38:45 +0200

    [scalapatternmatching] replaced println with check, added more descriptions, added KATA

---
+++ src/main/scala/jug/lodz/workshops/Workshops.scala
@@ -0,0 +1,15 @@
+package jug.lodz.workshops
+
+/**
+  * Created by pawel on 15.05.16.
+  */
+object Workshops {
+
+  def check[A](label:String, actual:A ,expected:A):Unit = actual match {
+    case `expected` =>println(s"  * in $label value is correct : $expected")
+    case _=>   println(s"  * in $label value is NOT CORRECT! expected : $expected but actual is $actual")
+  }
+
+
+
+}
--- src/main/scala/jug/lodz/workshops/fppatternmatching/exercises/PMPart1Syntax.scala
+++ src/main/scala/jug/lodz/workshops/fppatternmatching/exercises/PMPart1Syntax.scala
@@ -1,5 +1,7 @@
 package jug.lodz.workshops.fppatternmatching.exercises
 
+import jug.lodz.workshops.Workshops
+import jug.lodz.workshops.Workshops.check
 import jug.lodz.workshops.fppatternmatching.answers.PMPart1SyntaxAnswer.ExerciseLevel2.ExerciseLevel3
 
 import scala.annotation.tailrec
@@ -9,119 +11,11 @@ import scala.annotation.tailrec
   */
 object PMPart1Syntax {
 
-
-  object Demonstration {
-
-    def demo(): Unit = {
-      println("[DEMONSTRATION]")
-      println("\n *MATCH BY VALUE")
-      val someValue: Int = 5 //check what happens when given value is not in the pattern matching
-      someValue match {
-        case 1 => println("  -->value is one")
-        case 5 => println("  -->value is five")
-      }
-
-      println("\n *DEFAULT VALUE")
-
-      val someValue2: Int = 2
-      someValue2 match {
-        case 1 => println("  -->value is one")
-        case 5 => println("  -->value is five")
-        case _ => println("  -->some other value")
-      }
-
-      println("\n *VARIABLE IN PM & GUARDS")
-      val someValue3 = 3
-      someValue3 match {
-        case even if (even % 2 == 0) => println(s"  -->value is even : $even")
-        case odd => println(s"  -->value is odd : $odd")
-      }
-
-      println("\n *MATCHING LIST")
-      val someValues = List(1, 2, 3, 4, 5, 6)
-      someValues match {
-        case first :: second :: third :: tail => println(s"  --> $first , $second , $third, $tail")
-        case oneElement :: Nil => println(s"  --> this list has only one element  : $oneElement")
-        case _ => println("  --> list is empty") // comment this line and check warning , why compiler can notice that some combinations are missing?
-      }
-
-    }
-  }
-
-  object ExerciseLevel1 {
-    def exercise11() = {
-      val value1 = 1
-      val result1 = value1 match {
-        case 1 => ???
-        case 2 => 2 + 2
-        case _ => 100
-      }
-
-      println(s" *EXERCISE11 : ${result1 == 3}")
-    }
-
-    def exercise12() = {
-      def matchOnelement(element: Int) = element match {
-        // use guard if
-        case e if (e % 2 == 0) => ???
-      }
-
-      val result2 = List(1, 2, 3, 4, 5).map(matchOnelement) //map will apply matchOneElement function on each element
-      println(s""" *EXERCISE12 : ${result2 == List("odd", "even", "odd", "even", "odd")}""")
-    }
-  }
-
-  object ExerciseLevel2 {
-    def exercise21() = {
-      def firstElementOrZero(l: List[Int]): Int = l match {
-        // this is internal method // check for Nil -> empty list
-        case Nil => ???
-        case head :: tail => ???
-      }
-
-      println(s" *EXERCISE21 : ${firstElementOrZero(List(1, 2, 3)) == 1}")
-      println(s" *EXERCISE21 : ${firstElementOrZero(List(2, 3)) == 2}")
-      println(s" *EXERCISE21 : ${firstElementOrZero(List()) == 0}")
-    }
-
-
-    def exercise22() = {
-      //reverts the list only if it has three elements
-      def revertListIfSizeThree[A](l: List[A]): List[A] = ???
-
-      println(s" *EXERCISE22 : ${revertListIfSizeThree(List(1, 2, 3)) == List(3, 2, 1)}")
-      println(s" *EXERCISE22 : ${revertListIfSizeThree(List('a', 'b', 'c')) == List('c', 'b', 'a')}")
-      println(s" *EXERCISE22 : ${revertListIfSizeThree(List(1, 2, 3, 4)) == List(1, 2, 3, 4)}")
-    }
-
-
-    object ExerciseLevel3 {
-      def exercise31() = {
-        //use recursion
-        def size[A](list: List[A]): Int = ???
-
-        println(s" *EXERCISE31 : ${size(List(1, 2, 3)) == 3}")
-        println(s" *EXERCISE31 : ${size(List(1, 2, 3, 3, 3)) == 5}")
-        println(s" *EXERCISE31 : ${size(List('a', 'b')) == 2}")
-      }
-
-      def exercise32() = {
-        //@tailrec
-        def lastElement[A](list: List[A]): Option[A] = ???
-
-        println(s" *EXERCISE32 : ${lastElement(List(1, 2, 3)) == Some(3)}")
-        println(s" *EXERCISE32 : ${lastElement(List(1, 2, 5, 7, 88, 7)) == Some(7)}")
-        println(s" *EXERCISE32 : ${lastElement(List()) == None}")
-      }
-    }
-
-  }
-
   def main(args: Array[String]) {
     Demonstration.demo()
     //    println("[------EXERCISES------]")
     //    println("\n[------EXERCISE LEVEL 1------]")
-    //    ExerciseLevel1.exercise11()
+    //        ExerciseLevel1.exercise11()
     //    ExerciseLevel1.exercise12()
     //
     //    println("\n[------EXERCISE LEVEL 2------]")
@@ -132,5 +26,136 @@ object PMPart1Syntax {
     //    ExerciseLevel3.exercise31()
     //    ExerciseLevel3.exercise32()
   }
+
+
+  object Demonstration {
+
+    /**
+      * x - patern matching on simple values
+      * x - missing matches
+      * x - pattern matching on lists
+      */
+    def demo(): Unit = {
+      println("[DEMONSTRATION]")
+      println("\n *MATCH BY VALUE")
+      val someValue: Int = 5 //check what happens when given value is not in the pattern matching
+//      someValue match {
+//        case 1 => println("  -->value is one")
+//        case 5 => println("  -->value is five")
+//      }
+
+      println("\n *DEFAULT VALUE")
+
+      val someValue2: Int = 2
+//      someValue2 match {
+//        case 1 => println("  -->value is one")
+//        case 5 => println("  -->value is five")
+//        case _ => println("  -->some other value")
+//      }
+
+      println("\n *VARIABLE IN PM & GUARDS")
+      val someValue3 = 3
+//      someValue3 match {
+//        case even if (even % 2 == 0) => println(s"  -->value is even : $even")
+//        case odd => println(s"  -->value is odd : $odd")
+//      }
+
+      println("\n *MATCHING LIST")
+      val someValues = List(1, 2, 3, 4, 5, 6)
+//      someValues match {
+//        case first :: second :: third :: tail => println(s"  --> $first , $second , $third, $tail")
+//        case oneElement :: Nil => println(s"  --> this list has only one element  : $oneElement")
+//        case _ => println("  --> list is empty") // comment this line and check warning , why compiler can notice that some combinations are missing?
+//      }
+
+    }
+  }
+
+  /**
+    * Learning points :
+    * x pattern matching on simple types
+    * x handling missing values
+    * x pattern matching on types
+    */
+  object ExerciseLevel1 {
+    //result1 must equal 3
+    def exercise11() = {
+      val value1 = 1
+      val result1 = value1 match {
+        case 1 => ???
+        case 2 => 2 + 2
+        case _ => 100
+      }
+
+      check("EXERCISE11",result1,3)
+    }
+    // x - mapping on lists
+    // x - use guards
+    // List(1) -> List("odd")
+    // List(1,2) -> List("odd","even")
+    // List(1,2,3) -> List("odd","even","odd")
+    def exercise12() = {
+      def matchOnelement(element: Int) = element match {
+        // use guard if
+        case e if (e % 2 == 0) => ???
+      }
+
+      val result2 = List(1, 2, 3, 4, 5).map(matchOnelement) //map will apply matchOneElement function on each element
+      check("EXERCISE12",result2,List("odd", "even", "odd", "even", "odd"))
+    }
+  }
+
+  object ExerciseLevel2 {
+    // get first element of list or zero if list is empty
+    def exercise21() = {
+      def firstElementOrZero(l: List[Int]): Int = l match {
+        // this is internal method // check for Nil -> empty list
+        case Nil => ???
+        case head :: tail => ???
+      }
+
+      check("EXERCISE21",firstElementOrZero(List(1, 2, 3)),1)
+      check("EXERCISE21",firstElementOrZero(List(2, 3)),2)
+      check("EXERCISE21",firstElementOrZero(List()),0)
+    }
+
+
+    def exercise22() = {
+      //reverts the list only if it has three elements
+      def revertListIfSizeThree[A](l: List[A]): List[A] = ???
+
+      check("EXERCISE22",revertListIfSizeThree(List(1, 2, 3)),List(3, 2, 1))
+      check("EXERCISE22",revertListIfSizeThree(List('a', 'b', 'c')),List('a', 'b', 'c'))
+      check("EXERCISE22",revertListIfSizeThree(List(1, 2, 3, 4)),List(1, 2, 3, 4))
+    }
+
+
+    object ExerciseLevel3 {
+      def exercise31() = {
+        //use recursion to calculate size of the list
+        def size[A](list: List[A]): Int = ???
+
+        check("EXERCISE31",size(List(1, 2, 3)),3)
+        check("EXERCISE31",size(List(1, 2, 3, 3, 3)),5)
+        check("EXERCISE31",size(List('a', 'b')),2)
+
+      }
+
+      def exercise32() = {
+        //return last element of the list - USE PATTERN MATCHING
+        //return Some(x) if list is not empty
+        //return None if list is empty
+        //@tailrec
+        def lastElement[A](list: List[A]): Option[A] = ???
+
+        check("EXERCISE32",lastElement(List(1, 2, 3)),Some(3))
+        check("EXERCISE32",lastElement(List(1, 2, 5, 7, 88, 7)),Some(7))
+        check("EXERCISE32",lastElement(List()),None)
+      }
+    }
+
+  }
+
+
 
 }
