56ee127c9cecad9616b044a023ef62c5aeee8d32
pawel <pawel@domena.pl>
2016-07-09 08:25:49 +0200

    [functions] part 2 function as a value finished

---
+++ src/main/scala/jug/lodz/workshops/functions/answers/FunctionsPart2FunctionAsValueAnswer.scala
@@ -0,0 +1,92 @@
+package jug.lodz.workshops.functions.answers
+
+/**
+  * Created by pawel on 04.07.16.
+  */
+object FunctionsPart2FunctionAsValueAnswer {
+
+
+  def main(args: Array[String]) {
+    println("\n\n FUNCTIONS AS VALUE \n")
+
+    println(" * DRY & MODULARISATION")
+
+    val es=Seq(1,2,3,4,5)
+
+    def sum(init:Int, es:Seq[Int]): Int ={
+        var result=init //var is bad but here it is encapsulated and fucntion is short
+        for(e <- es) result=result+e
+        result
+    }
+
+    println("    * sum : "+sum(0,es))    // why init == 0  ?
+
+    def multiply(init:Int, es:Seq[Int]): Int ={
+      var result=init
+      for(e <- es) result=result*e   //only one difference
+      result
+    }
+
+    println("    * multiply : "+multiply(1,es))   // why init == 1 ?
+
+    def reduce(init:Int,es:Seq[Int], op: (Int,Int)=>Int): Int ={
+      var result=init
+      for(e <- es) result=op(result,e)   //only one difference
+      result
+    }
+
+    println("    * sum by reduce : "+reduce(0,es,(e1,e2)=>e1+e2))   // underscore-emoticon version reduce(0,es,_+_)
+    println("    * multiply by reduce : "+reduce(1,es,(e1,e2)=>e1*e2))   // underscore-emoticon version reduce(0,es,_*_)
+
+    println("    * from library : "+es.reduce((e1,e2)=>e1+e2))
+    println("    * from library : "+es.reduce((e1,e2)=>e1*e2 ))
+
+    def reduceGeneric[A](es:Seq[A],init:A, op: (A,A)=>A): A ={
+      var result:A=init
+      for(e <- es) result=op(result,e)   //only one difference
+      result
+    }
+
+    val joinString:(String,String) => String =  (s1,s2) => s1+" : "+s2
+    val strings=Seq("1","2","3")
+
+    println("    * reduce generic : "+reduceGeneric(strings,"",joinString))
+
+    println(" * A FUNCTION RECEIVING A FUNCTION")
+
+    val ints=Seq(1,2,3,4,5)
+    val mapReduceClosure: (Int=>Int) => Int = f=>ints.map(f).reduce(_+_)
+
+
+    println("    * mapReduceClosure (x+1) "+mapReduceClosure(e=>e+1))
+    println("    * mapReduceClosure (x*x) "+mapReduceClosure(e=>e*e))
+
+    val mapReduce: Seq[Int] => (Int=>Int) => Int = elements=> f=>elements.map(f).reduce(_+_)
+
+    println("    * mapReduce (1,2,3,4,5) (x+1) "+mapReduce(ints)(e=>e+1))
+    println("    * mapReduce (10,20,30) (x+1) "+mapReduce(Seq(10,20,30))(e=>e+1))
+
+
+    println(" * *** LOAN PATTERN ***")
+
+    //a simple function easy to test
+    val countLines:Iterator[String] => Int = it=>it.size
+
+    println(s"    * simple test counting lines : "+countLines(Iterator("line1","line2","line3")))
+
+    //loan method
+    def loanFile[A](path:String,f:Iterator[String]=>A): A ={
+      val source = scala.io.Source.fromFile(path)
+      try{
+            f(source.getLines())
+      }finally{
+        source.close()
+      }
+    }
+
+    //change path to your own
+    val linesInFile=loanFile("/tmp/file.txt",countLines)
+    println(s"    * lines in file : $linesInFile");
+
+  }
+}
---
+++ src/test/scala/jug/lodz/workshops/functions/exercises/FunctionsPart2FunctionAsValueExercises.scala
@@ -0,0 +1,122 @@
+package jug.lodz.workshops.functions.exercises
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by pawel on 08.07.16.
+  */
+class FunctionsPart2FunctionAsValueExercises extends FunSuite with Matchers {
+
+
+  test("test specific map") {
+    val elements = Seq("word", "longer", "sh")
+    val length: String => Int = s => s.length // or _.length
+
+    val result = specificMap(elements, length)
+
+    result should contain theSameElementsAs Seq(4, 6, 2)
+  }
+
+  def specificMap(seq: Seq[String], f: String => Int): Seq[Int] = {
+    //1) explain we are using mutable structure
+    //2) encapsulation
+    //3) keep this in exercises
+    val buffer: ArrayBuffer[Int] = new ArrayBuffer(seq.size)
+
+    //for (??? <- ???) buffer += ??? // += element  .append(element)
+
+    buffer.toSeq
+  }
+
+
+  test("specific filter") {
+    val elements = Seq("word", "longer", "sh", "three")
+
+    val result = specificFilter(elements, s => s.length > 4)
+
+    result should contain theSameElementsAs Seq("longer", "three")
+  }
+
+  def specificFilter(seq: Seq[String], predicate: String => Boolean): Seq[String] = {
+    val buffer: ArrayBuffer[String] = new ArrayBuffer() // no initial size
+
+    /// for (??? <- ???) ???
+
+    buffer.toSeq
+  }
+
+
+  test("build in map and filter") {
+    val parseInt : String => Int = ???
+    val square = (i:Int) => ???
+
+    val mappingFunction: String => Int = parseInt andThen square
+
+    val filteringFunction: Int => Boolean = _ > 10
+
+    val result = List("1", "2", "3", "4", "5")
+      .map(mappingFunction)
+      .filter(filteringFunction)
+
+    result should contain theSameElementsAs (List(16, 25))
+  }
+
+  test("function receiving function"){
+    val executeWithTwo: (Int=>Int) => Int = ???
+    val reportExecution: (Int => Int ) => String = ???
+
+    // implement execute with two so the result of result2(x->x*5) is 10
+    val result1: Integer = executeWithTwo.apply(x => x * 5)
+
+    //implement report execution so that apply(x->x*5) is "RESULT : 10"
+    val result2: String = reportExecution.apply(x => x * 5)
+
+    result1 shouldBe 10
+    result2 shouldBe "RESULT : 10"
+  }
+
+
+  //Level2
+  test("generic map"){
+    val transactions: List[(String, BigDecimal)] = List(
+      ("t1", BigDecimal("20")),
+      ("t2", BigDecimal("30")),
+      ("t3", BigDecimal("60"))
+    )
+
+    val tax=BigDecimal("0.23")
+
+
+    val netMoney: Seq[BigDecimal] =genericMap(transactions, (t:(String,BigDecimal))=>t._2)
+    val grossMoney: Seq[BigDecimal] =genericMap(netMoney, (m:BigDecimal)=> m +  m * tax)
+
+    grossMoney should contain theSameElementsAs Seq(BigDecimal("24.60"),BigDecimal("36.90"),BigDecimal("73.80"))
+  }
+
+
+
+  def genericMap[A,B](seq: Seq[A], f: A => B): Seq[B] = {
+   ???
+  }
+
+  test("generic filter"){
+    val transactions: List[(String, BigDecimal)] = List(
+      ("t1", BigDecimal("20")),
+      ("t2", BigDecimal("30")),
+      ("t1", BigDecimal("60"))
+    )
+
+    val result=genericFilter(transactions, (t:(String,BigDecimal))=>t._1=="t1")
+
+    result should contain theSameElementsAs Seq(
+      ("t1", BigDecimal("20")),
+      ("t1", BigDecimal("60"))
+    )
+  }
+
+  def genericFilter[A](seq: Seq[A], predicate: A => Boolean): Seq[A] = {
+    ???
+  }
+}
