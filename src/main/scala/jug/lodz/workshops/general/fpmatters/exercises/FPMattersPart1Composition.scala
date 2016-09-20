9aa999d76d2e23b30bd75233929a471dd4a46d3a
pawel <pawel@domena.pl>
2016-09-20 19:05:34 +0200

    [fpmatters] functional composition

--- src/main/scala/jug/lodz/workshops/general/fpmatters/exercises/FPMattersPart1Composition.scala
+++ src/main/scala/jug/lodz/workshops/general/fpmatters/exercises/FPMattersPart1Composition.scala
@@ -3,7 +3,7 @@ package jug.lodz.workshops.general.fpmatters.exercises
 /**
   * Created by pawel on 18.09.16.
   */
-object FPMattersPart1Intro {
+object FPMattersPart1Composition {
 
   def main(args: Array[String]): Unit = {
     println("\n\n Functional Programming Matters Intro \n")
@@ -11,6 +11,8 @@ object FPMattersPart1Intro {
     println("  *** Compose Functions ***")
     case class Product(name:String, price:BigDecimal)
     val tv = Product("Tv", BigDecimal(100.0))
+    val pc = Product("Pc", BigDecimal(120.0))
+    val mouse = Product("Mouse", BigDecimal(30.0))
 
     val getPrice:Product => BigDecimal = p=>p.price
     val gross : BigDecimal => BigDecimal = net => net * 1.23
@@ -43,10 +45,10 @@ object FPMattersPart1Intro {
 
     println("  *** Fold Right ***")
 
-    val sum=List(1,2,3,4,5).foldRight(0)((e,acc)=>acc+e)
+    val sum:List[Int]=>Int=l=>l.foldRight(0)((e,acc)=>acc+e)
 
 // CODE
-//    println("      *  fold right sum: "sum)
+//    println("      *  fold right sum: "+sum(List(1,2,3,4,5)))
 
     println("  \nfold right step by step")
 //    List(1,2,3,4,5).foldRight(0){(e,acc)=>
@@ -57,6 +59,23 @@ object FPMattersPart1Intro {
     println("  *** Map ***")
     val mapped = List(1, 2, 3, 4, 5).map(e => e + 1)
 //    println(mapped)
+
+
+    println("  *** Map with foldr ***")
+    def mapByFoldr[A,B]:(A=>B) =>List[A]=>List[B]= f=>input=>
+        input.foldRight(List.empty[B]){(e,acc)=>
+          f(e)::acc
+        }
+
+//    println(s"      *  map with foldr : "+mapByFoldr(getPrice)(List(tv,pc,mouse)))
+
+
+    println("  *** Final Composition : foldr,currying,compose ***")
+
+    val productSum: List[Product] => Int =mapByFoldr(getPrice) andThen mapByFoldr(price=>price.toInt) andThen sum
+
+//    println(s"      *  product sum : "+ productSum(List(tv,pc,mouse)))
+
   }
 
 }
