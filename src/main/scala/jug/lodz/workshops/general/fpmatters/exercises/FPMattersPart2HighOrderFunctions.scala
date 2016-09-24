2ea0c8968cd0f68208a775037a6003295b57ff5e
pawel <pawel@domena.pl>
2016-09-24 21:30:20 +0200

    [fpmatters] part 2 - high order functions

---
+++ src/main/scala/jug/lodz/workshops/general/fpmatters/exercises/FPMattersPart2HighOrderFunctions.scala
@@ -0,0 +1,95 @@
+package jug.lodz.workshops.general.fpmatters.exercises
+
+/**
+  * Created by pawel on 18.09.16.
+  */
+object FPMattersPart2HighOrderFunctions {
+
+  def main(args: Array[String]): Unit = {
+    println("\n\n Functional Programming Matters High Order Functions \n")
+
+    println("  *** How To use list in recursion ***")
+    val l1=List(1,2,3)
+    val l2=1::2::3::Nil
+// CODE - how to do recursion
+//    println(s"      *  l1==l2 : ${l1==l2}")
+//
+//    val head::tail=l2
+//    println(s"      *  first head {$head} and first tail $tail")
+//    val head2::tail2=tail
+//    println(s"      *  second head {$head2} and second tail $tail2")
+//    val head3::tail3=tail2
+//    println(s"      *  third head {$head3} and third tail $tail3")
+
+    println("  *** Generic Parameterization ***")
+    def loop[A](l:List[A])(zero:A)(f: (A,A)=>A):A = l match{
+      case Nil =>  zero
+      case head::tail => f(head,loop(tail)(zero)(f))
+    }
+
+// CODE - less generic functions with loop
+//    val sum:List[Int]=>Int= list=> loop(list)(0){(e,acc)=>
+//      e+acc
+//    }
+//
+//    val sumShort=(l:List[Int]) => loop(l)(0)(_+_)
+//
+//    println("      *  sum with loop: "+sum(List(1,2,3,4,5)))
+
+
+    println("  *** compose parameterization ***")
+
+    val unitSum:Int=>Int=>Int = a=>b=>a+b
+    val unitSquare:Int=>Int = i=>i*i
+
+
+// CODE compose simple functions
+//    val unitSumSquared: (Int, Int) => Int = Function.uncurried(unitSquare andThen unitSum)
+
+//    val sumSquared:List[Int] => Int = l=> loop(l)(0)(unitSumSquared)
+//
+//   println("      *  sum squared with loop : "+sumSquared(List(1,2,3)))
+
+    println("  *** different types ***")
+    case class Product(name:String, price:BigDecimal)
+    val tv = Product("Tv", BigDecimal(10.0))
+    val pc = Product("Pc", BigDecimal(20.0))
+    val mouse = Product("Mouse", BigDecimal(30.0))
+
+    val getPrice:Product => BigDecimal = p=>p.price
+    val getName:Product => String = p=>p.name
+    val gross : BigDecimal => BigDecimal = net => net * 1.23
+
+    object MathLibrary{
+      val sum: (BigDecimal, BigDecimal) => BigDecimal = (bd1, bd2)=> bd1+bd2
+      val max: (BigDecimal, BigDecimal) => BigDecimal = (bd1, bd2)=> if(bd1> bd2) bd1 else bd2
+    }
+
+    object CSVLib{
+      val joinCsvLine:(Any,Any) => String = (a,b) => if (""!=b) a+","+b else a.toString
+    }
+
+    def generalLoop[A,B](l:List[A])(zero:B)(f: (A,B)=>B):B = l match{
+      case Nil =>  zero
+      case head::tail => f(head,generalLoop(tail)(zero)(f))
+    }
+
+    //MANIPULATE CONDITION INDEPENDENTLY!!
+    val grossPrice: (Product) => BigDecimal =getPrice andThen gross  // try to comment out gross , how much code need to change
+    val sumGrossPrice: (Product) => (BigDecimal) => BigDecimal = grossPrice andThen MathLibrary.sum.curried //change sum to max
+    val conditionUncurried: (Product, BigDecimal) => BigDecimal = Function.uncurried(sumGrossPrice)
+
+    //COMPOSITION WITH DIFFERENT TYPES
+    //    val buildCsv=Function.uncurried(grossPrice andThen CSVLib.joinCsvLine.curried)
+    //    val buildNamesCsv=Function.uncurried(getName andThen CSVLib.joinCsvLine.curried)
+    //("")(buildCsv)
+
+    //different types
+//    val compositionResult=generalLoop(List(tv,pc,mouse))(BigDecimal(0))(conditionUncurried)
+//    println("      *  sum gross prices : "+compositionResult)
+
+
+  }
+
+
+}
---
+++ src/test/scala/jug/lodz/workshops/general/fpmatters/exercises/FPMattersPart2HighOrderFunctionsAnswers.scala
@@ -0,0 +1,102 @@
+package jug.lodz.workshops.general.fpmatters.exercises
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by pawel on 19.09.16.
+  */
+class FPMattersPart2HighOrderFunctionsAnswers extends FunSuite with Matchers{
+
+  //implement those customCurry and customUncurry - don't change test
+  test("custom curry & uncurry"){
+    val original : (Int,Int) => Int = (i1,i2) => i1+i2
+    val curried = customCurry(original)
+    val uncurried=customUnCurry(curried)
+
+    original(3,4) shouldBe 7
+    curried(3)(4) shouldBe 7
+    uncurried(3,4) shouldBe 7
+  }
+
+  def customCurry[A,B,C](f:(A,B)=>C):A=>B=>C = ???
+  def customUnCurry[A,B,C](f:A=>B=>C):(A,B)=>C = ???
+
+
+  /**** FOLDR  *****/
+
+  //notice changed parameters order - first function then collection so you need
+  //to explicitly declare function types
+  def customFoldR[A,B](f: (A,B)=>B)(zero:B)(l:List[A]):B = l match{
+    case Nil =>  zero
+    case head::tail => f(head,customFoldR(f)(zero)(tail))
+  }
+
+
+  //implement filterByFoldR - don't change the test
+  test("filter by custom foldr"){
+      import ProductsDao._
+      val products=ProductsDao.selectProducts()
+      filterByFoldR((p:Product)=>p.price>30)(products) shouldBe List(car,pc)
+  }
+
+  //use customFolR to implement filter
+  def filterByFoldR[A](p:A=>Boolean)(l:List[A]):List[A]=customFoldR(???)(???)(???)
+
+
+  //implement mapByFoldR - don't change the test
+  test("map by custom foldr"){
+    val products=ProductsDao.selectProducts()
+
+    mapByFoldR((p:Product)=>p.name)(products) shouldBe List("Car","Pc","Banana")
+  }
+
+  //use customFolR to implement map
+  def mapByFoldR[A,B](f:A=>B)(l:List[A]):List[B] = customFoldR(???)(???)(???)
+
+
+  //define missin piece and add it to the composition
+  //notice how having function as a first parameter of high order make easier to build chain of computation
+  test("higher kind functions composition"){
+    val getName:Product=>String = _.name
+    val getPrice:Product=>BigDecimal = _.price
+    val greaterThan : BigDecimal => BigDecimal => Boolean = threeshold => x => x > threeshold
+    val joinString:String=>(String,String) => String = delimiter => (a,b) => if (b.nonEmpty) a+delimiter+b else a
+    val toProductsJson:String=>String = productsLine => s"""{"products" : [${productsLine}]}"""
+
+    val premiumProducts: List[Product] => List[Product] =filterByFoldR(getPrice andThen greaterThan(30))
+    val productsNames: List[Product] => List[String] =mapByFoldR(getName)
+    val joinstringLine: List[String] => String = customFoldR(joinString(","))("")
+
+    //run this test once before uncommenting this line
+//    val missingPiece : List[String] => List[String] = ???
+
+    val listPremiumProducts = premiumProducts andThen productsNames andThen joinstringLine andThen toProductsJson
+
+
+    listPremiumProducts(ProductsDao.selectProducts()) shouldBe """{"products" : ["Car","Pc"]}"""
+  }
+
+  //intro to part 3 - working with infinity
+  //implement iterate - don't change the test
+  test("towards infinity"){
+    val oddNumbersSequence: (Int) => List[Int] =iterate{ (i:Int)=>i+2}(1)
+
+    oddNumbersSequence(7) shouldBe List(1,3,5,7,9,11,13)
+  }
+
+  //simple if is enough
+  def iterate[A](f:A=>A)(a:A)(n:Int):List[A]= ???
+
+  //Domain
+  case class Product(name:String, price:BigDecimal)
+
+  object ProductsDao{
+    val car = Product("Car", BigDecimal(1000.0))
+    val pc = Product("Pc", BigDecimal(50.0))
+    val banana = Product("Banana", BigDecimal(10.0))
+
+    def selectProducts():List[Product] = List(car,pc,banana)
+
+  }
+
+}
