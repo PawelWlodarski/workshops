448893b0e50304f3c31154f689568951480c00da
pawel <pawel@domena.pl>
2018-06-27 07:59:49 +0200

    code for presentation

--- src/main/scala/jug/presentations/fpintro/FPCombinators.scala
+++ src/main/scala/jug/presentations/fpintro/FPCombinators.scala
@@ -4,23 +4,22 @@ object FPCombinators {
 
   trait Optional[A]
 
-  def unit[A](a:A) :Optional[A] = ???
-  def flatMap[A,B](fa:Optional[A])(f : A=>Optional[B] ) : Optional[B] = ???
+  def unit[A](a: A): Optional[A] = ???
+  def flatMap[A, B](fa: Optional[A])(f: A => Optional[B]): Optional[B] = ???
 
 
+  def map[A, B](fa: Optional[A])(f: A => B): Optional[B] =
+    flatMap(fa)(a => unit(f(a)))
 
-//    def  map[A,B](fa:Optional[A])(f : A=>B ) : Optional[B] =
-//      flatMap(fa)(a=>unit(f(a)))
-//
-//      fun <A,B,C> map2(oa:Optional<A>,ob:Optional<B>)(f : (A,B) ->C ) : Optional<C> =
-//        flatMap(oa)(a->map(b)(f(a,b)))
-//
-//        fun <A,B,C> compose(fa:A→Optional<B>,fb:B→Optional<C>):A→Optional<C> =
-//          a->flatmap(fa(a))(fb)
-//
-//          fun  <A> sequence(lma: List<Option<A>>): Option<List<A>> = …
-//
-//            fun  <A,B> traverse(la: List<A>)(f: A => Optional<B>): Optional<List<B>> =
-//
-//              }
+  def map2[A, B, C](oa: Optional[A], ob: Optional[B])(f: (A, B) => C): Optional[C] =
+    flatMap(oa)(a => map(ob)(b => f(a, b)))
+
+  def compose[A, B, C](fa: A => Optional[B], fb: B => Optional[C]): A => Optional[C] =
+    a => flatMap(fa(a))(fb)
+
+  def sequence[A](lma: List[Option[A]]): Option[List[A]] = ???
+
+  def traverse[A, B](la: List[A])(f: A => Optional[B]): Optional[List[B]] = ???
+
+
 }
